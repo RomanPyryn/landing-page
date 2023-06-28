3163ef5b8cb61f6978788f5fc0c9f86f4dc141ca
RomanPyryn <92987401+RomanPyryn@users.noreply.github.com>
2023-06-28 18:58:03 +0300

    add title

--- gulp/tasks/deploy.js
+++ gulp/tasks/deploy.js
@@ -1,7 +1,7 @@
-var gulp = require('gulp');
-var ghPages = require('gulp-gh-pages');
+var gulp = require("gulp");
+var ghPages = require("gulp-gh-pages");
 
-gulp.task('deploy', function() {
-  return gulp.src('./dist/**/*')
-    .pipe(ghPages());
-});
+export const deploy = () => {
+	return gulp.src("./dist/**/*").pipe(ghPages());
+}
+
